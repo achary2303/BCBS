7ba292b0cf53dd911547c01872055d8ece4e9b5a
KeerthiT30 <45610624+KeerthiT30@users.noreply.github.com>
2019-04-21 23:21:22 -0400

    Add files via upload
    Tests which developed till now.

---
+++ tests.py
@@ -0,0 +1,13 @@
+# pages/tests.py
+from django.http import HttpRequest
+from django.test import SimpleTestCase
+#from django.urls import reverse
+
+#from . import views
+
+
+class HomePageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = selfs.client.get('/')
+        self.assertEqual(response.statuxs_code, 200)
